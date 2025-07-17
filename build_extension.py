c5f1d3094694d531ab481b263610685694cda543
Boris Bugreev <boris_bugreev@mail.ru>
2025-07-17 14:57:44 +0300

    feat: add cffi builder for future C extensions

---
+++ build_extension.py
@@ -0,0 +1,33 @@
+# This program describes the C library builder.
+from cffi import FFI
+import pathlib
+
+ffibuilder = FFI()
+
+# Preparing to get paths to C files
+project_name = "pysatl_tsp"
+c_sources = "c"
+current_dir = pathlib.Path(__file__).parent
+project_dir = current_dir / project_name
+c_def = ''
+headers = ''
+src = []
+
+# Read and get C headers
+for item in project_dir.rglob("*.h"):
+    with open(item, "r") as f:
+        c_def += f.read()
+        headers += f"#include \"{item.relative_to(project_dir)}\"\n"
+
+# Get list of path to all *.c files
+for item in (project_dir / c_sources).rglob("*.c"):
+    src.append(str(item.relative_to(current_dir)))
+
+
+# Set options to compile C library
+ffibuilder.cdef(c_def)
+ffibuilder.set_source(f"{project_name}._c", headers, sources=src)
+
+# Compile C library
+if __name__ == "__main__":
+    ffibuilder.compile(verbose=True)
