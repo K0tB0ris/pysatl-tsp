a16829f9409845f4daebb4cf48467847bdb20073
Boris Bugreev <boris_bugreev@mail.ru>
2025-07-30 12:26:27 +0300

    feat: add compile options to c lib builder

--- build_extension.py
+++ build_extension.py
@@ -9,7 +9,7 @@ project_name = "pysatl_tsp"
 c_sources = "c"
 current_dir = pathlib.Path(__file__).parent
 project_dir = current_dir / project_name
-c_def = ''
+c_def = """"""
 headers = ''
 src = []
 
@@ -19,14 +19,20 @@ for item in project_dir.rglob("*.h"):
         c_def += f.read()
         headers += f"#include \"{item.relative_to(project_dir)}\"\n"
 
+print("CDEF \n")
+print(c_def)
+
+print("\n HEADERS \n")
+print(headers)
+
 # Get list of path to all *.c files
 for item in (project_dir / c_sources).rglob("*.c"):
     src.append(str(item.relative_to(current_dir)))
 
-
 # Set options to compile C library
 ffibuilder.cdef(c_def)
-ffibuilder.set_source(f"{project_name}._c", headers, sources=src)
+ffibuilder.set_source(f"{project_name}._c", headers, sources=src,
+                      extra_compile_args=['-fno-omit-frame-pointer', '-Wall', '-Wextra'])
 
 # Compile C library
 if __name__ == "__main__":
